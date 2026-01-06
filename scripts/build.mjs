dee0d8791eb4a36cc94e8f7eb9991cb2174533de
琴音ふぁみ <fami@ktnfm.com>
2026-01-06 21:19:27 +0900

    ワークフローって大抵一発じゃ動かない

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,45 +1,48 @@
-import { cp, readFile, writeFile } from "node:fs/promises";
+import { cp, mkdir, readFile, writeFile } from "node:fs/promises";
 import { build } from "esbuild";
 
-const manifest = JSON.parse(await readFile("manifest.common.json", "utf-8"));
-Object.assign(
-    manifest,
-    JSON.parse(await readFile(`manifest.${process.argv[2] === "firefox" ? "firefox" : "chromium"}.json`)),
-);
-writeFile("dist/manifest.json", JSON.stringify(manifest), "utf-8");
+(async function() {
+    const manifest = JSON.parse(await readFile("manifest.common.json", "utf-8"));
+    Object.assign(
+        manifest,
+        JSON.parse(await readFile(`manifest.${process.argv[2] === "firefox" ? "firefox" : "chromium"}.json`)),
+    );
+    await mkdir("dist", { recursive: true });
+    await writeFile("dist/manifest.json", JSON.stringify(manifest), "utf-8");
 
-cp("src/html/action.html", "dist/action.html");
-cp("src/html/options.html", "dist/options.html");
+    await cp("src/html/action.html", "dist/action.html");
+    await cp("src/html/options.html", "dist/options.html");
 
-build({
-    entryPoints: ["src/index.content.ts"],
-    outfile: "dist/index.content.js",
-    platform: "browser",
-    format: "cjs",
-    bundle: true,
-    minify: true,
-});
-build({
-    entryPoints: ["src/index.action.ts"],
-    outfile: "dist/index.action.js",
-    platform: "browser",
-    format: "cjs",
-    bundle: true,
-    minify: true,
-});
-build({
-    entryPoints: ["src/index.options.ts"],
-    outfile: "dist/index.options.js",
-    platform: "browser",
-    format: "cjs",
-    bundle: true,
-    minify: true,
-});
-build({
-    entryPoints: ["src/index.background.ts"],
-    outfile: "dist/index.background.js",
-    platform: "browser",
-    format: "cjs",
-    bundle: true,
-    minify: true,
-});
+    build({
+        entryPoints: ["src/index.content.ts"],
+        outfile: "dist/index.content.js",
+        platform: "browser",
+        format: "cjs",
+        bundle: true,
+        minify: true,
+    });
+    build({
+        entryPoints: ["src/index.action.ts"],
+        outfile: "dist/index.action.js",
+        platform: "browser",
+        format: "cjs",
+        bundle: true,
+        minify: true,
+    });
+    build({
+        entryPoints: ["src/index.options.ts"],
+        outfile: "dist/index.options.js",
+        platform: "browser",
+        format: "cjs",
+        bundle: true,
+        minify: true,
+    });
+    build({
+        entryPoints: ["src/index.background.ts"],
+        outfile: "dist/index.background.js",
+        platform: "browser",
+        format: "cjs",
+        bundle: true,
+        minify: true,
+    });
+})();
